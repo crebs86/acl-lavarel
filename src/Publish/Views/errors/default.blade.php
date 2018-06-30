55b02d484dd68006225795fdcfa0cc01ce4be601
crebs86 <crebs.m@gmail.com>
2018-06-30 15:06:17 -0300

    Add files via upload

---
+++ src/Publish/Views/errors/default.blade.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: crebs
+ * Date: 22/04/18
+ * Time: 17:17
+ */
+?>
+@extends('layouts.panel_bt_40')
+@section('content')
+    <link href="{{url('css/bootstrap.css')}}" rel="stylesheet">
+    <div class="col col-sm-12">
+        <h1>Você parece estar perdido. Tente voltar</h1>
+    </div>
+@endsection
