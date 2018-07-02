fdc80dc324df1c85368d1d31daad0f64d30cc26e
crebs86 <crebs.m@gmail.com>
2018-07-01 22:41:12 -0300

    Create helpers.php

---
+++ src/helpers.php
@@ -0,0 +1,54 @@
+<?php
+
+use Crebs86\Acl\Controllers\Util\ControlAccess;
+
+if (!function_exists('have')) {
+    /**
+     * @param $permission
+     * @param bool $needSuperAdmin
+     * @return void
+     */
+    function have($permission, $needSuperAdmin = true)
+    {
+        $have = new ControlAccess();
+        $have->check($permission, $needSuperAdmin)->on();
+    }
+}
+
+if (!function_exists('can')) {
+    /**
+     * @param $permission
+     * @param bool $needSuperAdmin
+     * @return bool
+     */
+    function can($permission, $needSuperAdmin = true)
+    {
+        $can = new ControlAccess();
+        return $can->check($permission, $needSuperAdmin)->get();
+    }
+}
+
+if (!function_exists('own')) {
+    /**
+     * @param $permission
+     * @param $collection
+     * @return void
+     */
+    function own($permission, $collection)
+    {
+        $isOwn = new ControlAccess();
+        $isOwn->check($permission)->self($collection);
+    }
+}
+if (!function_exists('isOwn')) {
+    /**
+     * @param $permission
+     * @param $collection
+     * @return bool
+     */
+    function isOwn($permission, $collection)
+    {
+        $isOwn = new ControlAccess();
+        return $isOwn->check($permission)->isOwn($collection);
+    }
+}
